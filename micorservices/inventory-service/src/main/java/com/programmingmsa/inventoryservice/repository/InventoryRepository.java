9e19b36bad5d9b6a5fe9e24f5e8ab1987508d3ee
wooseokoh <allsafe1206@gmail.com>
2022-12-22 18:41:47 +0900

    01_003. inventory-service complete

---
+++ micorservices/inventory-service/src/main/java/com/programmingmsa/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.programmingmsa.inventoryservice.repository;
+
+import com.programmingmsa.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
