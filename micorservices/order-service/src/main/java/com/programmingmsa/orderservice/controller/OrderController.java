a7c9eaf8273e36c88833c1b9cc40b2b9cdfc4296
wooseokoh <allsafe1206@gmail.com>
2022-12-22 16:55:43 +0900

    01_002. order-service complete

---
+++ micorservices/order-service/src/main/java/com/programmingmsa/orderservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.programmingmsa.orderservice.controller;
+
+import com.programmingmsa.orderservice.dto.OrderRequest;
+import com.programmingmsa.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+}
---
+++ micorservices/order-service/src/main/java/com/programmingmsa/orderservice/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.programmingmsa.orderservice.service;
+
+import com.programmingmsa.orderservice.dto.OrderLineItemsDto;
+import com.programmingmsa.orderservice.dto.OrderRequest;
+import com.programmingmsa.orderservice.model.Order;
+import com.programmingmsa.orderservice.model.OrderLineItems;
+import com.programmingmsa.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
