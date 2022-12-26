97431261918eaf1eda4b12ccf9e8c1a6ac72db86
wooseokoh <allsafe1206@gmail.com>
2022-12-26 23:29:54 +0900

    03. API Gateway complete

---
+++ micorservices/api-gateway/src/test/java/com/programmingmsa/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingmsa.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
