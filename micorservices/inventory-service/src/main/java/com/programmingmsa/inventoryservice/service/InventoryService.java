282f19af578889dd647dc9db291a967650277c84
wooseokoh <allsafe1206@gmail.com>
2022-12-28 23:47:44 +0900

    05. Resilience4J Circuit Breaker setting

--- micorservices/inventory-service/src/main/java/com/programmingmsa/inventoryservice/service/InventoryService.java
+++ micorservices/inventory-service/src/main/java/com/programmingmsa/inventoryservice/service/InventoryService.java
@@ -3,11 +3,14 @@ package com.programmingmsa.inventoryservice.service;
 import com.programmingmsa.inventoryservice.dto.InventoryResponse;
 import com.programmingmsa.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
@@ -15,7 +18,11 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wail Start");
+        Thread.sleep(10000);
+        log.info("Wail Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                         InventoryResponse.builder()
